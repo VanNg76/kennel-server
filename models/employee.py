998ad524294975ae510a36f039189d99aee1ffc7
Van Nguyen <thanh_xer@yahoo.com>
2022-04-07 15:29:29 -0500

    finish create class, starts SQL reading

---
+++ models/employee.py
@@ -0,0 +1,8 @@
+class Employee():
+    """Class initialize
+    """
+    def __init__(self, id, name, email, employee):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.employee = employee
